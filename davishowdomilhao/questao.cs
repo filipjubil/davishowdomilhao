46f26faa7436ab25671923d1eb38746479cb7097
davi <davicamachom@gmail.com>
2024-09-17 09:01:59 -0300

    continiou

---
+++ davishowdomilhao/questao.cs
@@ -0,0 +1,47 @@
+namespace davishowdomilhao;
+public class questao
+{
+public string pergunta1;
+public string resposta2;
+public string resposta3;
+public string resposta4;
+public string resposta5;
+public int respostacorreta;
+public int nivel;
+label labelpergunta;
+Button buttonResposta1;
+Button buttonResposta2;
+Button buttonResposta3;
+Button buttonResposta4;
+Button buttonResposta5;
+
+public void Desenhar()
+{
+    labelpergunta.text=pergunta1;
+    buttonResposta1.Text=buttonResposta1;
+    buttonResposta1.Text=buttonResposta2;
+    buttonResposta1.Text=buttonResposta3;
+    buttonResposta1.Text=buttonResposta4;
+    buttonResposta1.Text=buttonResposta5;
+
+}
+public Boll verifica Resposta (int rr)
+{
+    if (resposta Certa == rr)
+    {
+        var btn = Q
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+}
